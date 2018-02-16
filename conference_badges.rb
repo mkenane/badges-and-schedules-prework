a3347f6360deb7a707254e88482090bd95752722
Mor Kenane <morkenan@hotmail.com>
2018-02-16 00:31:34 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,7 +6,6 @@ def batch_badge_creator(array)
   array.map {|name| "Hello, my name is #{name}."}
 end 
 
-
 def batch_badge_creator(array)
   array.each_with_index.map {|name, i| "Hello, #{name}! You'll be assigned to room #{i+1}!" }
 end 
